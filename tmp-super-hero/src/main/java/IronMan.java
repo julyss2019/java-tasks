489ecc58d3a72e585ce8643cdaf19d0a3adc0db9
july_ss_2019 <july_ss_2017@outlook.com>
2023-11-07 09:33:12 +0800

    feat: update

--- tmp-super-hero/src/main/java/IronMan.java
+++ tmp-super-hero/src/main/java/IronMan.java
@@ -1,5 +1,28 @@
-public class IronMan  extends SuperHero {
+public class IronMan extends SuperHero {
+    private String weapon = "钢铁战甲";
+
     public IronMan() {
         super("钢铁侠");
+    }
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(String weapon) {
+        this.weapon = weapon;
+    }
+
+    @Override
+    public void introduce() {
+        System.out.printf("****** %s ******\n" +
+                "我是: %s\n" +
+                "我的武器是: %s\n" +
+                "我的职业是: %s\n" +
+                "我的任务是: %s\n" +
+                "我的指挥部是: %s\n", getName(), getName(), weapon, getOccupation(), getTask(), getHeadquarters());
+        attack();
+        ability();
+        contact();
     }
 }
--- tmp-super-hero/src/main/java/SuperHero.java
+++ tmp-super-hero/src/main/java/SuperHero.java
@@ -1,7 +1,7 @@
 public class SuperHero {
     private String name = "超级英雄";
     private String occupation = "维护世界和平";
-    private String task ="维护世界和平";
+    private String task = "打败洛基";
     private String headquarters = "S.H.I.E.L.D";
 
     public SuperHero(String name) {
@@ -37,6 +37,26 @@ public class SuperHero {
     }
 
     public void introduce() {
-        System.out.printf("****** %s ******\n我是: 超级英雄\n我的职业是: %s\n我的任务是: %s\n我的指挥部是: %s\n", name, occupation, task, headquarters);
+        System.out.printf("****** %s ******\n" +
+                "我是: 超级英雄\n" +
+                "我的职业是: %s\n" +
+                "我的任务是: %s\n" +
+                "我的指挥部是: %s\n", name, occupation, task, headquarters);
+    }
+
+    public void attack() {
+        System.out.println("我的攻击行为");
+    }
+
+    protected void ability() {
+        System.out.println("我的能力介绍");
+    }
+
+    void contact() {
+        System.out.println("我的联络方式");
+    }
+
+    private void recruit() {
+        System.out.println("快来加入");
     }
 }
