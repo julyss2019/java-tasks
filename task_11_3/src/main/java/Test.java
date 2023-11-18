da733b7ae13883d819f12946b76c546b35e931d4
void <july_ss_2017@outlook.com>
2023-11-18 14:24:00 +0800

    feat: update task 11-3

--- task_11_3/src/main/java/Test.java
+++ task_11_3/src/main/java/Test.java
@@ -5,7 +5,8 @@ public class Test {
         Class clazz = new Class();
 
         clazz.addStudent(new Undergraduate("00001", "小明", Arrays.asList(50.5D, 60D, 70D)));
-        clazz.addStudent(new Graduate("00002", "小红", Arrays.asList(100D, 70D, 80D)));
+        clazz.addStudent(new Graduate("00002", "小红", Arrays.asList(50.5D, 60D, 70D)));
+        clazz.addStudent(new Graduate("00003", "小白", Arrays.asList(50.5D, 52D, 67D)));
 
         for (Student student : clazz.getStudents()) {
             System.out.printf("%s(%s): %s%n", student.getName(), student.getId(), student.calculateLevel());
