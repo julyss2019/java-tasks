2046a906d44dc2fa16ead0287676c84422c495d9
july_ss_2019 <july_ss_2017@outlook.com>
2023-11-21 09:33:19 +0800

    feat: task 13-3

--- task-13-3/src/main/java/Test.java
+++ task-13-3/src/main/java/Test.java
@@ -2,7 +2,7 @@ public class Test {
     public static void main(String[] args) {
         test(new Circle(10));
         test(new Rectangle(3, 4));
-        test(new Triangle(3, 4));
+        test(new Triangle(3, 4)); // 仅考虑直角三角形
     }
 
     private static void test(Shape shape) {
